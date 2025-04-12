09b2e1e25563fdd3a8510e56c8d29e31abc4f4df
ilya <ilya@yandex.ru>
2025-04-12 21:16:32 +0300

    Add stylelint

--- webapp/src/components/plantCard/plantCard.tsx
+++ webapp/src/components/plantCard/plantCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material'
 import { Link } from 'react-router-dom'
 import { Tplant } from '../../../../types'
 import * as routes from '../../lib/routes'
@@ -5,12 +6,15 @@ import s from './plantCard.module.scss'
 
 export function PlantCard(plant: Tplant) {
   return (
-    <Link className={s.link} to={routes.getPlantProfileRoute({ plantId: plant.plantId })}>
-      <div className={s.card}>
-        <h1 className={s.genus}>{plant.genus}</h1>
-        <h1 className={s.spesies}>{plant.spesies}</h1>
-        <h1 className={s.description}>{[plant.description]}</h1>
-      </div>
-    </Link>
+    <div className={s.card}>
+      <h1 className={s.genus}>{plant.genus}</h1>
+      <h1 className={s.spesies}>{plant.spesies}</h1>
+      <h1 className={s.description}>{[plant.description]}</h1>
+      <Link className={s.link} to={routes.getPlantProfileRoute({ plantId: plant.plantId })}>
+        <Button className={s.button} variant="outlined" color="secondary">
+          перейти
+        </Button>
+      </Link>
+    </div>
   )
 }
