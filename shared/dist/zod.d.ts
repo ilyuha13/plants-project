70a1f371b3a4ea72cc0d76ee8bb0b9db8afcaa5d
ilya <ilya@yandex.ru>
2025-11-10 10:38:44 +0300

    Fix: delete shared/dist from git. add shared/.gitignore

--- shared/dist/zod.d.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod'
-export declare const zNonemptyTrimmed: z.ZodString
-export declare const zNonemptyTrimmedRequiredOnNotLocal: z.ZodEffects<
-  z.ZodOptional<z.ZodString>,
-  string | undefined,
-  string | undefined
->
-//# sourceMappingURL=zod.d.ts.map
--- shared/dist/zod.js
+++
@@ -1,6 +0,0 @@
-import { z } from 'zod'
-export const zNonemptyTrimmed = z.string().trim().min(1)
-export const zNonemptyTrimmedRequiredOnNotLocal = zNonemptyTrimmed
-  .optional()
-  .refine((val) => process.env.HOST_ENV === 'local' || !!val, 'Required on local host')
-//# sourceMappingURL=zod.js.map
