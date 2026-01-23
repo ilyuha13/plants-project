53faea0b22b05e50d9e03775c62885f89bbb69ee
Ilya Churshukov <ilyachurshukov13@gmail.com>
2026-01-23 21:15:12 +0300

    Fix: The ability to enter uppercase characters in the name field of the registration form has been added

--- backend/src/router/signUp/input.ts
+++ backend/src/router/signUp/input.ts
@@ -4,6 +4,6 @@ export const zSignUpTrpcInput = z.object({
   nick: z
     .string()
     .min(5, 'минимум 5 символов')
-    .regex(/^[a-z0-9-]+$/, 'только буква латинского алфавита, цифры и тире'),
+    .regex(/^[A-Za-z0-9-]+$/, 'только буква латинского алфавита, цифры и тире'),
   password: z.string().min(5),
 })
